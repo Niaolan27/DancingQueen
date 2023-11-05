0c1751179827e052b14a6a339f52df91742cd625
volatileknight <77604203+volatileknight@users.noreply.github.com>
2023-11-05 01:31:47 -0400

    Update game.py

--- src/game.py
+++ src/game.py
@@ -2,6 +2,7 @@ import math
 import random
 import mediapipe as mp
 import cv2
+import decimal
 import timeit
 
 class Game:
@@ -9,36 +10,45 @@ class Game:
         self.isRunning = True
         self.score = 0
         self.rectList = []
-        self.rectSize = 100
+        self.rectSize = 50
         self.right_wrist_x = 0
         self.right_wrist_y = 0
         self.left_wrist_x = 0
         self.left_wrist_y = 0
         
     def updateScore(self, poseResults):
+        print(f'Wrist location{poseResults}')
+        print(self.rectList)
          # boolean variable for whether the whole program is running
         for wristLocation in poseResults:
+            
             for rectangle in self.rectList:
                 x1 = rectangle[0]
                 x2 = rectangle[1]
                 y1 = rectangle[2]
                 y2 = rectangle[3]
-                if (x1 < wristLocation[0] < x2) and (y1 < wristLocation[1] < y2): # using the bounding box to find the intersection
-                    self.removeRectangle(rectangle)
+                if (x1 < wristLocation[1] < x2) and (y1 < wristLocation[0] < y2): # using the bounding box to find the intersection
+                    self.rectList.remove(rectangle)
                     self.score += 5
 
+                    
     def generateRect(self, currentTime, dt): # add the interval in which to add coords
-        x1 = random.randint(0, 400)
+        x1 = random.randint(0, 350)
         x2 = x1 + self.rectSize
-        y1 = random.randint(0, 400)
+        y1 = random.randint(0, 350)
         y2 = y1 + self.rectSize
         rectCoords = [x1, x2, y1, y2]
-        if currentTime % dt == 0:
-            self.rectList.append[rectCoords]
+        
+        currentTime = decimal.Decimal(str(currentTime))
+        dt = decimal.Decimal(str(dt))
+        result = currentTime % dt
+        if result == 0:
+            self.rectList.append(rectCoords)
             
     
     def getRectList(self):
-     return self.rectList
+        return self.rectList
+    
     
     def poseDetection(self, frame):
     # Convert the BGR image to RGB
