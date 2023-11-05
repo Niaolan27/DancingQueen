1a39e9b068f9be23f430a682fd875aef078278ff
volatileknight <77604203+volatileknight@users.noreply.github.com>
2023-11-05 01:30:37 -0400

    Update music.py

--- src/music.py
+++ src/music.py
@@ -4,24 +4,42 @@ import mediapipe as mp
 import cv2
 import timeit
 import librosa
+import pygame
 
 class Music:
     def __init__(self, title):
-        self.songDict = {'I knew you were trouble - Taylor Swift': file1, 'Sorry - Justin Beiber': file2, 'Dancing Queen - ABBA': file3, "Hips don't lie - Shakira": file4, "Something just like this - Coldplay": file5}
+        self.songDict = {"I Knew You Were Trouble - Taylor Swift": 'C:\\Users\\wands\\Documents\\CS_15112\\hack112\\taylor_swift-trouble.mp3',
+                         "Sorry - Justin Beiber": 'C:\\Users\\wands\\Documents\\CS_15112\\hack112\\justin_bieber-sorry.mp3',
+                         "Dancing Queen - ABBA": 'C:\\Users\\wands\\Documents\\CS_15112\\hack112\\abba-dancing.mp3',
+                         "Hips Don't Lie - Shakira": 'C:\\Users\\wands\\Documents\\CS_15112\\hack112\\shakira-hips_dont_lie.mp3',
+                         "Something Just Like This - Coldplay": 'C:\\Users\\wands\\Documents\\CS_15112\\hack112\\chainsmokers-coldplay.mp3'}
         self.bpm = 0
         self.duration = 0
         self.audioFile = ""
+        print(title)
+        self.title = title
+        
     
-    def getAudioFile(self, title):
-        self.audioFile = self.songDict[title]
+    def getAudioFile(self):
+        self.audioFile = self.songDict[self.title]
         return self.audioFile
 
-    def getMusicStats(self, title):
-        audioFile = self.getAudioFile(title) # 'random.mp3'
+    def getMusicStats(self):
+        audioFile = self.getAudioFile() # 'random.mp3'
         y, sr = librosa.load(audioFile)
 
-        duration = librosa.get_duration(y=y, sr=sr)
-        tempoBPM = librosa.beat.beat_track(y=y, sr=sr)
-        return (duration, tempoBPM) 
+        self.duration = librosa.get_duration(y=y, sr=sr)
+        self.bpm = librosa.beat.beat_track(y=y, sr=sr)[0]
+        return (self.duration, self.bpm)
+     
+    def loadMusic(self):
+        pygame.init()
+        pygame.mixer.music.load(self.audioFile)
+
+    def start(self):
+        pygame.mixer.music.play(loops=1)
+
+    def stop(self):
+        pygame.mixer.music.stop()
     
     
